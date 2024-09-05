6ce80e1d6db43bd60d5034273ee18a3ba10c0872
talesrizzi09 <talesrizzi1@gmail.com>
2024-09-05 00:36:54 -0300

    ultimo commit tentando hospedar

--- api/index.js
+++ api/index.js
@@ -1,25 +1,3 @@
-const express = require('express');
-const app = express();
+import app from '../src/api';
 
-// Configuração básica do middleware
-app.use(express.json());
-
-// Definição das suas rotas
-app.get('/api/sala/entrar', (req, res) => {
-    res.send('Rota de entrada funcionando!');
-});
-
-app.get('/api/salas', (req, res) => {
-    res.send('Listar todas as salas');
-});
-
-// Exportando o app para ser usado pelo Vercel
-module.exports = app;
-
-// Exporta para o Vercel
-if (require.main === module) {
-    const PORT = process.env.PORT || 5000;
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
-}
+export default app;
