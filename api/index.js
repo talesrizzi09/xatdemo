0e339baf9e7d8fe88094b1c0451d73eceeb25795
talesrizzi09 <talesrizzi1@gmail.com>
2024-09-05 00:26:07 -0300

    ultimo commit tentando hospedar

--- api/index.js
+++ api/index.js
@@ -1,2 +1,12 @@
-import app from '../src/api';
-export default app;
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+// Rota exemplo
+app.get('/api/sala/entrar', (req, res) => {
+    res.send('Rota de entrada funcionando!');
+});
+
+// Exporta o app para a Vercel
+module.exports = app;
